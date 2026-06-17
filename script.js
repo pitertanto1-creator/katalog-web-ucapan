d3eef7866a6d1ac8c19536a43f216c658d336261
Kedai Nandung <pitertanto1@gmail.com>
2026-06-17 11:09:22 +0700

    feat: make KadoLink homepage compact

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-const templateCards = document.querySelectorAll(".template-card, .process-card, .price-card");
-
-const revealOnScroll = () => {
-  templateCards.forEach((card) => {
-    const cardTop = card.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
-
-    if (cardTop < windowHeight - 80) {
-      card.classList.add("show");
-    }
-  });
-};
-
-window.addEventListener("scroll", revealOnScroll);
-window.addEventListener("load", revealOnScroll);
+console.log("KadoLink ready");
