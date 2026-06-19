d01559b486dfb792bbee6464df6a4b7772bac139
Kedai Nandung <pitertanto1@gmail.com>
2026-06-19 14:55:37 +0700

    docs: add birthday retro windows product guides

---
+++ core/retro/birthday-retro-windows/data.js
@@ -0,0 +1,72 @@
+window.BIRTHDAY_DATA = {
+  websiteTitle: "Birthday Surprise",
+  name: "Someone Special",
+
+  welcomeTitle: "Welcome to<br>Birthday Surprise!",
+  welcomeText:
+    "Hari ini ada kejutan kecil<br>buat kamu.<br>Klik OK dulu ya,<br>pelan-pelan kita buka satu per satu.",
+
+  songTitle: "Your Favorite Song",
+  songTime: "Now Playing",
+  musicFile: "assets/music/birthday-song.mp3",
+  coverImage: "assets/images/foto1.jpg",
+  coverText: "For You ♥",
+
+  musicMessage: [
+    { text: "Hari ini spesial banget.", pink: true },
+    {
+      text:
+        "Bukan cuma karena ulang tahun,<br>tapi karena kamu pantas dirayain."
+    },
+    {
+      text:
+        "Semoga hal-hal baik makin sering datang,<br>senyum kamu makin banyak,<br>dan capeknya pelan-pelan diganti bahagia."
+    },
+    { text: "Enjoy your day yaa 💖", pink: true }
+  ],
+
+  noteMessage: [
+    { text: "💗 Hai kamu,", pink: true },
+    { text: "Hari ini aku cuma mau bilang:" },
+    { text: "Selamat ulang tahun yaa! 🎂", pink: true },
+    {
+      text:
+        "Semoga kamu selalu dikelilingi hal-hal baik,<br>orang-orang yang tulus,<br>dan alasan kecil buat senyum setiap hari."
+    },
+    {
+      text:
+        "Jangan lupa bangga sama diri sendiri,<br>karena kamu sudah sejauh ini."
+    },
+    {
+      text:
+        "Semoga wish kamu satu-satu dikabulin,<br>pelan-pelan tapi pasti."
+    },
+    { text: "Pokoknya hari ini kamu harus happy! ✨", pink: true }
+  ],
+
+  aboutMessage: [
+    { text: "Tentang kamu:", pink: true },
+    {
+      text:
+        "Kamu itu tipe orang yang mungkin sederhana,<br>tapi kehadirannya bisa bikin hari seseorang jadi lebih hangat."
+    },
+    {
+      text:
+        "Semoga di umur baru ini,<br>kamu makin sayang sama diri sendiri,<br>makin kuat, dan makin banyak bahagianya."
+    }
+  ],
+
+  photos: [
+    "assets/images/foto1.jpg",
+    "assets/images/foto2.jpg",
+    "assets/images/foto3.jpg",
+    "assets/images/foto4.jpg"
+  ],
+
+  alertText: "Ada kejutan kecil buat kamu!",
+  reminderText: "Siap-siap senyum ya :)",
+  finalBirthdayText:
+    "Semoga hari ini jadi salah satu hari yang kamu ingat dengan senyum.",
+  finalBirthdaySubtext:
+    "Happy birthday, stay happy, and keep shining! 🎂✨"
+};
